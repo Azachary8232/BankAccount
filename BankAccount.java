28b2dbd50a03f88517e1bed10c76b7ba64868354
Azachary8232 <90228565+Azachary8232@users.noreply.github.com>
2021-11-30 18:51:11 -0700

    Added Constructor

--- BankAccount.java
+++ BankAccount.java
@@ -6,4 +6,12 @@ public class BankAccount{
     double savingsBalance;
     static int numberOfAccounts = 0;
     static double accountsTotal = 0;
+
+
+    public BankAccount(){
+        numberOfAccounts++;
+    }
+
+
+
 }
